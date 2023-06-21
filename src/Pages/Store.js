f96495cc19336843952f786df45d21d06aaa1574
Rachel Morris <rachelmorris262@outlook.com>
2023-06-21 20:49:28 +0100

    set up input box test, and currently setting up input box

--- src/Pages/Store.js
+++ src/Pages/Store.js
@@ -4,6 +4,11 @@ function Store() {
       <header>
         <h1 className="text-3xl font-bold mt-6 text-center text-teal-blue">myCupboard</h1>
       </header>
+      <form>
+
+      <input className="p-4 mt-8 ml-6 text-lg" variant="static" placeholder="New Item" />
+
+      </form>
     </div>
   );
 }
--- src/Pages/Store.test.js
+++ src/Pages/Store.test.js
@@ -1,9 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import Store from './Store';
 
-test('renders learn react link', () => {
+test('checks app name is present', () => {
   render(<Store />);
   
   const linkElement = screen.getByText(/myCupboard/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+describe("<Store />", () => {
+
+  test('Check that a input box is present', () => {
+    render(<Store/>);
+
+    const inputEl = screen.getByTestId("food-input");
+    expect(inputEl).toBeInTheDocument();
+    expect(inputEl).toHaveAttribute("type", "Noodles");
+  });
+});
+
